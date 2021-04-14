9552512916630d77b01b22edf5947d98b70585e3
Kolmagin Danil <dkolmagin@gmail.com>
2021-04-14 15:50:33 +0300

    Copying from base repo

---
+++ integration/balancer_test.go
@@ -0,0 +1,27 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+const baseAddress = "http://balancer:8090"
+
+var client = http.Client{
+	Timeout: 3 * time.Second,
+}
+
+func TestBalancer(t *testing.T) {
+	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
+	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+}
+
+func BenchmarkBalancer(b *testing.B) {
+	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+}
